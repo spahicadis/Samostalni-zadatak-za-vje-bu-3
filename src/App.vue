b409ac241e0d226e2123ab812cd692131a6f8abc
spahicadis <adis@spahica.com>
2025-04-17 14:44:56 +0200

    Setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import WebKosarica from './components/WebKosarica.vue'
+</script>
+
+<template>
+  <div class="h-screen w-full p-5 flex items-center justify-center">
+    
+    <WebKosarica/>
+
+  </div>
+</template>
+
+
---
+++ src/components/WebKosarica.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, reactive } from 'vue'
+const proizvodi = reactive([{
+
+}])
+const inputNaziv = ref("")
+const inputCijena = ("")
+//computed za ukupno
+//funkcija ukloni, dodaj, povecaj, smanji kolicinu
+
+</script>
+
+
+<template>
+
+
+<div class="min-w-lg h-fit p-4 flex flex-col gap-1.5 rounded-md shadow-md bg-gray-200">
+
+  
+</div>
+
+
+
+
+
+</template>
